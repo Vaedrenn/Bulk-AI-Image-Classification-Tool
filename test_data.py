f7557ac5e169bb94c4bdad34a6bcf147329b01db
Vaedrenn <brianto10622@gmail.com>
2024-01-13 01:38:01 -0500

    Added test for rating

--- test_data.py
+++ test_data.py
@@ -79,3 +79,33 @@ def test_predict_all(model, labels):
         assert all_results
         assert rating_results
         assert text
+
+
+# tests if the ratings and labels match up
+def test_predict_rating(model, labels):
+    img_path = r'tests/images/ヒトこもる_花心_97031695_p0.png'
+    _, height, width, _ = model.input_shape
+
+    # Model only supports 3 channels
+    image = PIL.Image.open(img_path).convert('RGB')
+
+    image = np.asarray(image)
+    image = tf.image.resize(image,
+                            size=(height, width),
+                            method=tf.image.ResizeMethod.AREA,
+                            preserve_aspect_ratio=True)
+    image = image.numpy()
+    image = dd.image.transform_and_pad_image(image, width, height)
+    image = image / 255.
+
+    threshold_results, all_results, rating, text = predict(model, labels, image, 0.5)
+
+    # Check if any key in rating is in threshold_results
+    common_keys = set(rating.keys()).intersection(threshold_results.keys())
+
+    for key in common_keys:
+        if threshold_results[key] == rating[key]:
+            print(f"Key: {key}, Threshold Value: {threshold_results[key]}, Rating Value: {rating[key]}")
+
+    # Assert that all common keys have the same values
+    assert all(threshold_results[key] == rating[key] for key in common_keys)
