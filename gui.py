dcdffa9f283d6cad897f61e0a8b2ba4538ab4181
Vaedrenn <brianto10622@gmail.com>
2024-01-28 02:03:33 -0500

    scope changes, submit button function

--- gui.py
+++ gui.py
@@ -7,107 +7,6 @@ from PyQt5.QtCore import Qt
 import CheckListWidget
 
 
-def action_box_widget(widget: QWidget) -> QWidget:
-    action_box = QWidget(widget)
-    action_layout = QVBoxLayout()
-    action_box.setLayout(action_layout)
-
-    selection_frame = QWidget()
-    slider_frame = QWidget()
-    button_frame = QWidget()
-    output_frame = QWidget()
-
-    selection_grid = QGridLayout()
-    slider_grid = QGridLayout()
-    button_grid = QGridLayout()
-    output_grid = QGridLayout()
-
-    selection_frame.setLayout(selection_grid)
-    slider_frame.setLayout(slider_grid)
-    button_frame.setLayout(button_grid)
-    output_frame.setLayout(output_grid)
-
-    action_layout.addWidget(selection_frame)
-    action_layout.addWidget(slider_frame)
-    action_layout.addWidget(button_frame)
-
-    model_input = QLineEdit()
-    model_input.setPlaceholderText("Select model directory...")
-    dir_input = QLineEdit()
-    dir_input.setPlaceholderText("Select directory...")
-    model_button = QPushButton("Browse")
-    dir_button = QPushButton("Browse")
-
-    model_button.clicked.connect(lambda value: browse_model(widget, model_input))
-    dir_button.clicked.connect(lambda value: browse_directory(dir_input))
-
-    selection_grid.addWidget(model_input, 0, 0)
-    selection_grid.addWidget(model_button, 0, 1)
-    selection_grid.addWidget(dir_input, 1, 0)
-    selection_grid.addWidget(dir_button, 1, 1)
-
-    general_tag = QLabel("General Tags Threshold")
-    character_tag = QLabel("Character Tags Threshold")
-    general_slider = QSlider(Qt.Horizontal)
-    character_slider = QSlider(Qt.Horizontal)
-    general_threshold = QSpinBox()
-    character_threshold = QSpinBox()
-
-    general_slider.setMinimum(0)
-    general_slider.setMaximum(100)
-    general_slider.setTickInterval(1)
-    character_slider.setMinimum(0)
-    character_slider.setMaximum(100)
-    character_slider.setTickInterval(1)
-
-    general_threshold.setMinimum(0)
-    general_threshold.setMaximum(100)
-    character_threshold.setMinimum(0)
-    character_threshold.setMaximum(100)
-
-    general_slider.valueChanged.connect(lambda value: general_threshold.setValue(value))
-    character_slider.valueChanged.connect(lambda value: character_threshold.setValue(value))
-    general_threshold.valueChanged.connect(lambda value: general_slider.setValue(value))
-    character_threshold.valueChanged.connect(lambda value: character_slider.setValue(value))
-
-    slider_grid.addWidget(general_tag, 0, 0)
-    slider_grid.addWidget(general_threshold, 0, 2)
-    slider_grid.addWidget(general_slider, 1, 0, 1, 3)
-    slider_grid.addWidget(character_tag, 2, 0)
-    slider_grid.addWidget(character_threshold, 2, 2)
-    slider_grid.addWidget(character_slider, 3, 0, 1, 3)
-
-    submit_button = QPushButton("Submit")
-    reset_button = QPushButton("Reset")
-
-    one_image_button = QPushButton("Tag Current Image")
-    all_images_button = QPushButton("Tag Selected images")
-
-    button_grid.addWidget(submit_button, 0, 0)
-    button_grid.addWidget(reset_button, 0, 1)
-    button_grid.addWidget(one_image_button, 3, 0)
-    button_grid.addWidget(all_images_button, 3, 1)
-
-    return action_box
-
-
-def browse_directory(line_edit):
-    directory_path = QFileDialog.getExistingDirectory(None, "Select Directory")
-    if directory_path:
-        line_edit.setText(directory_path)
-
-
-def browse_model(widget, line_edit):
-    directory_path = QFileDialog.getExistingDirectory(None, "Select Directory")
-    if not directory_path:
-        return
-    else:
-        print("importing actions")
-        from actions import load_model, load_labels
-        print("Finished importing actions")
-        line_edit.setText(directory_path)
-        widget.model = load_model(directory_path)
-        widget.labels = load_labels(directory_path)
 
 class MyGUI(QWidget):
     def __init__(self):
@@ -160,7 +59,7 @@ class MyGUI(QWidget):
         image_label_layout.addWidget(image_label)
         image_label.setAlignment(Qt.AlignCenter)
 
-        action_box = action_box_widget(self)
+        action_box = self.action_box_widget()
 
         text_output = QTextEdit()
         text_output.setPlaceholderText("Text Output")
@@ -188,6 +87,120 @@ class MyGUI(QWidget):
         self.setGeometry(100, 100, 1200, 800)
         self.setWindowTitle('PyQt Horizontal Layout with Labels and Borders')
         self.show()
+
+    def action_box_widget(self) -> QWidget:
+        action_box = QWidget(self)
+        action_layout = QVBoxLayout()
+        action_box.setLayout(action_layout)
+
+        selection_frame = QWidget()
+        slider_frame = QWidget()
+        button_frame = QWidget()
+        output_frame = QWidget()
+
+        selection_grid = QGridLayout()
+        slider_grid = QGridLayout()
+        button_grid = QGridLayout()
+        output_grid = QGridLayout()
+
+        selection_frame.setLayout(selection_grid)
+        slider_frame.setLayout(slider_grid)
+        button_frame.setLayout(button_grid)
+        output_frame.setLayout(output_grid)
+
+        action_layout.addWidget(selection_frame)
+        action_layout.addWidget(slider_frame)
+        action_layout.addWidget(button_frame)
+
+        model_input = QLineEdit()
+        model_input.setPlaceholderText("Select model directory...")
+        dir_input = QLineEdit()
+        dir_input.setPlaceholderText("Select directory...")
+        model_button = QPushButton("Browse")
+        dir_button = QPushButton("Browse")
+
+        model_button.clicked.connect(lambda value: self.browse_model(model_input))
+        dir_button.clicked.connect(lambda value: self.browse_directory(dir_input))
+
+        selection_grid.addWidget(model_input, 0, 0)
+        selection_grid.addWidget(model_button, 0, 1)
+        selection_grid.addWidget(dir_input, 1, 0)
+        selection_grid.addWidget(dir_button, 1, 1)
+
+        general_tag = QLabel("General Tags Threshold")
+        character_tag = QLabel("Character Tags Threshold")
+        general_slider = QSlider(Qt.Horizontal)
+        character_slider = QSlider(Qt.Horizontal)
+        general_threshold = QSpinBox()
+        character_threshold = QSpinBox()
+
+        general_slider.setMinimum(0)
+        general_slider.setMaximum(100)
+        general_slider.setTickInterval(1)
+        character_slider.setMinimum(0)
+        character_slider.setMaximum(100)
+        character_slider.setTickInterval(1)
+
+        general_threshold.setMinimum(0)
+        general_threshold.setMaximum(100)
+        character_threshold.setMinimum(0)
+        character_threshold.setMaximum(100)
+
+        general_slider.valueChanged.connect(lambda value: general_threshold.setValue(value))
+        character_slider.valueChanged.connect(lambda value: character_threshold.setValue(value))
+        general_threshold.valueChanged.connect(lambda value: general_slider.setValue(value))
+        character_threshold.valueChanged.connect(lambda value: character_slider.setValue(value))
+
+        slider_grid.addWidget(general_tag, 0, 0)
+        slider_grid.addWidget(general_threshold, 0, 2)
+        slider_grid.addWidget(general_slider, 1, 0, 1, 3)
+        slider_grid.addWidget(character_tag, 2, 0)
+        slider_grid.addWidget(character_threshold, 2, 2)
+        slider_grid.addWidget(character_slider, 3, 0, 1, 3)
+
+        submit_button = QPushButton("Submit")
+        reset_button = QPushButton("Reset")
+        submit_button.clicked.connect(lambda value: self.submit(dir_input.text()))
+
+        one_image_button = QPushButton("Tag Current Image")
+        all_images_button = QPushButton("Tag Selected images")
+
+        button_grid.addWidget(submit_button, 0, 0)
+        button_grid.addWidget(reset_button, 0, 1)
+        button_grid.addWidget(one_image_button, 3, 0)
+        button_grid.addWidget(all_images_button, 3, 1)
+
+        return action_box
+
+    def browse_directory(self, line_edit):
+        directory_path = QFileDialog.getExistingDirectory(None, "Select Directory")
+        if directory_path:
+            line_edit.setText(directory_path)
+
+    def browse_model(self, line_edit):
+        directory_path = QFileDialog.getExistingDirectory(None, "Select Directory")
+        if not directory_path:
+            return
+        else:
+            print("importing actions")
+            from actions import load_model, load_labels
+            print("Finished importing actions")
+            line_edit.setText(directory_path)
+            self.model = load_model(directory_path)
+            self.labels = load_labels(directory_path)
+
+    def submit(self, dir):
+        if self.model is None:
+            return
+        if self.labels is None or []:
+            # Warn user that model correctly loaded but no labels are found
+            return
+        if dir is not None:
+            return
+        from actions import predict_all
+        score_threshold = self.action_box.general_threshold / 100
+
+        results = predict_all(self.model, self.labels, dir, score_threshold)
 
 
 if __name__ == '__main__':
