1cfc187b155df0d7964b583da489133cab20fcad
vaedrenn <brianto10622@gmail.com>
2024-03-04 13:13:24 -0500

    added multiprocessing.freeze_support

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import multiprocessing
 import sys
 
 from PyQt5.QtWidgets import QApplication
 
 from src.gui.main_widget import MainWidget
 
-if __name__ == '__main__':
+def main():
     app = QApplication(sys.argv)
     myGUI = MainWidget()
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    main()
